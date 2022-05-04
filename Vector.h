28d422e920ba294a81be4fa3687f7fabd8ff59c8
Ananthkk2021 <96517754+Ananthkk2021@users.noreply.github.com>
2022-05-04 21:25:33 +0530

    cm

--- Vector.h
+++ Vector.h
@@ -6,11 +6,13 @@
 class Vector : public std::vector<double>{
 };
 
-inline void print(Vector &v){
+inline void print(const Vector &v){
     std::cout<<'[';
     for(auto i: v)
     {
-        std::cout<<i<<',';
+        std::cout<<i;
+        if(&i+1!=v.end())
+            std::cout<<',';
     }
     std::cout<<']'<<endl;
 }
