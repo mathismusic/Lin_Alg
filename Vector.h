d05ae2ef5816dcdf6daa6f0116fb626c3acfa977
mathismusic <69148989+mathismusic@users.noreply.github.com>
2022-05-04 21:13:19 +0530

    v2

--- Vector.h
+++ Vector.h
@@ -8,9 +8,7 @@ class Vector : public std::vector<double>{
 };
 
 inline void print(const Vector &v){
-//    std::cout << std::fixed;
-//    std::cout.precision(6);
-    for (const double &component : v.vec){
+    for (const double &component : v){
         std::cout.width(12); // need to set after every << call else default(of 0)is taken
         std::cout << component;
     }
