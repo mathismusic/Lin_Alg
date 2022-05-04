84574edf60b507971be0c7ef15993dfbf7abc3f8
mathismusic <69148989+mathismusic@users.noreply.github.com>
2022-05-04 21:11:25 +0530

    v3

--- matrix.h
+++ matrix.h
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
 
-template <class T>
+//template <class T>
 class Vector : public std::vector<double>{
 
 };
+
+inline void print(const Vector &v){
+//    std::cout << std::fixed;
+//    std::cout.precision(6);
+    for (const double &component : v.vec){
+        std::cout.width(12); // need to set after every << call else default(of 0)is taken
+        std::cout << component;
+    }
+    std::cout << std::endl;
+}
