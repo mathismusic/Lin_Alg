174b3b3c20a8183a3e826bdb13e0f38f35e3fe1d
mathismusic <69148989+mathismusic@users.noreply.github.com>
2022-05-09 21:19:53 +0530

    v2

--- matrix.h
+++ matrix.h
@@ -8,4 +8,8 @@ public:
     Matrix(){}
     Matrix(int m, int n): vector<Vector>(n,Vector(m))
     {}
+    Matrix(std::initializer_list<std::initializer_list<double> > init){
+        for (int i = 0; i < init.size(); i++)
+            at(i) = Vector(*(init.begin() + i));
+    }
 };
