5fc19e160c048da31a7af37d31f5d53d214ab12e
AnanthKidambi <106254340+AnanthKidambi@users.noreply.github.com>
2022-06-04 16:02:30 +0530

    dk

--- matrix.h
+++ matrix.h
@@ -190,12 +190,36 @@ inline void print(const Matrix &m){
     }
 }
 
-// class SquareMatrix: public Matrix{
-// public:
-//     friend inline double det(const SquareMatrix &s);
-// };
+class SquareMatrix: public Matrix{
+public:
+    friend inline double det(const SquareMatrix &s);
+    double det(int start_row, int start_col){
+        if (start_row == at(0).size() || start_col == size())
+        {
+            double x{1};
+            for(int i{0};i<order.first;i++)
+            {
+                x*=at(i,i);
+            }
+            return x;
+        }
+        bool allzero = true;
+        for (int column = start_col; column < size(); column++)
+            if (std::abs(at(start_row, column)) > EPSILON){
+                allzero = false;
+                at(start_col).swap(at(column));
+                break;
+            }
+        if (allzero) return 0;
+        double x = at(start_row, start_col);
+        for (int column = start_col + 1; column < size(); column++)
+            at(column) -= (at(start_row, column) * at(start_col))/x;
+        return det(start_col + 1, start_row + 1);
+    }
+};
 
 // inline double det(const SquareMatrix &s){
+//     SquareMatrix s{*this};
 //     if (start_row == at(0).size() || start_col == size()) return *this; // do nothing more
 //     bool allzero = true;
 //     for (int column = start_col; column < size(); column++)
